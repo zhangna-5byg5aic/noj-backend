000399e8a72ceabcf827c8b74db3751e1ddba546
zhangna_5byg5aic <zn92898@163.com>
2024-08-16 16:59:58 +0800

    完成代码沙箱架构，判题服务

---
+++ src/main/java/com/yupi/springbootinit/judge/codesandbox/impl/RemoteCodeSandbox.java
@@ -0,0 +1,15 @@
+package com.yupi.springbootinit.judge.codesandbox.impl;
+
+import com.yupi.springbootinit.judge.codesandbox.CodeSandbox;
+import com.yupi.springbootinit.judge.codesandbox.model.ExecuteCodeRequest;
+import com.yupi.springbootinit.judge.codesandbox.model.ExecuteCodeResponse;
+/**
+ * 远程代码沙箱（实际调用接口的沙箱）
+ */
+public class RemoteCodeSandbox implements CodeSandbox {
+    @Override
+    public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
+        System.out.println("远程代码沙箱");
+        return null;
+    }
+}
--- src/main/java/com/yupi/springbootinit/service/impl/QuestionSubmitServiceImpl.java
+++ src/main/java/com/yupi/springbootinit/service/impl/QuestionSubmitServiceImpl.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.yupi.springbootinit.common.ErrorCode;
 import com.yupi.springbootinit.constant.CommonConstant;
 import com.yupi.springbootinit.exception.BusinessException;
+import com.yupi.springbootinit.judge.JudgeService;
 import com.yupi.springbootinit.model.dto.questionsubmit.QuestionSubmitAddRequest;
 import com.yupi.springbootinit.model.dto.questionsubmit.QuestionSubmitQueryRequest;
 import com.yupi.springbootinit.model.entity.Question;
@@ -22,10 +23,12 @@ import com.yupi.springbootinit.utils.SqlUtils;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 /**
@@ -41,6 +44,9 @@ public class QuestionSubmitServiceImpl extends ServiceImpl<QuestionSubmitMapper,
 
     @Resource
     private UserService userService;
+    @Resource
+    @Lazy
+    private JudgeService judgeService;
 
     /**
      * 提交题目
@@ -78,7 +84,13 @@ public class QuestionSubmitServiceImpl extends ServiceImpl<QuestionSubmitMapper,
         if (!save){
             throw new BusinessException(ErrorCode.SYSTEM_ERROR, "数据插入失败");
         }
-        return questionSubmit.getId();
+        Long questionSubmitId = questionSubmit.getId();
+        // 执行判题服务
+        CompletableFuture.runAsync(() -> {
+            judgeService.doJudge(questionSubmitId);
+        });
+        return questionSubmitId;
+
     }
 
 
